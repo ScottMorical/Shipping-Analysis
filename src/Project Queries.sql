9d34f8c955302a3814c2e74b86cc8e1e6037f7b4
Scott <110702768+ScottMorical@users.noreply.github.com>
2024-06-18 08:50:00 -0700

    Update Project Queries.sql

--- src/Project Queries.sql
+++ src/Project Queries.sql
@@ -1,3 +1,25 @@
+# City with the most Gallons imported each month
+SELECT MonthDate, City
+	FROM(SELECT MONTH(PostingDate) as MonthDate,
+    ROW_NUMBER() OVER(PARTITION BY MONTH(PostingDate) ORDER BY SUM(Gallons)) as ranking, 
+    Ship_to_City as City
+	FROM data
+	GROUP BY Ship_to_City, MONTH(PostingDate)) as rankedcity
+WHERE ranking = 1;
+
+# Running Total Profit
+WITH rolling_rank AS (
+	SELECT 
+		MONTH(PostingDate) as Month_Date, 
+        SUM(Mat_Margin) as Profit, 
+		SUM(SUM(Mat_Margin)) OVER(PARTITION BY MONTH(PostingDate)
+		ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) as running_total
+	FROM data
+    GROUP BY MONTH(PostingDate))
+
+SELECT Month_Date, CONCAT("$", running_total) as running_total
+FROM rolling_rank;
+
 # Average Profit per City 
 SELECT Ship_To_City as "City", round(SUM(Mat_Margin)/COUNT(Distinct ShipID),1) as "Avg Material Profit per Shipment", round(Distance/1609,2) as Distance_Miles, 
 round(SUM(Mat_Margin)/SUM(Gallons),1) as "Avg Material Profit per Gallon"
